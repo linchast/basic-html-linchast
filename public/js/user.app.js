0f169094d9e0d495d0c999c0f057e6755477bee2
linchast <54513537+linchast@users.noreply.github.com>
2019-09-15 17:19:50 -0400

    Created JS file for users and updated HTML page for HW 3

---
+++ public/js/user.app.js
@@ -0,0 +1,27 @@
+var waitingApp = new Vue({
+  el: '#patientWaitingApp',
+  data: {
+    person: [
+      {
+
+      }
+    ]
+  },
+  methods: {
+    fetchPatients() {
+      fetch('https://randomuser.me/api/')
+      .then( response => response.json() )
+      .then( json => {waitingApp.person = json.results[0]})
+      ;
+
+      // Means the same at this
+      // fetch('https://randomuser.me/api/')
+      // .then(function(response) {return response.json()})
+      // .then(function(json) {waitingApp.people = json});
+
+    }
+  },
+  created() {
+    this.fetchPatients();
+  }
+})
